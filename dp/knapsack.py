267a0e037cdefaf5142b10480b1f266adfa275d7
Dhruv Kumar <dhruvlol12@gmail.com>
2025-03-01 07:19:55 +0530

    knapsack

---
+++ dp/knapsack.py
@@ -0,0 +1,14 @@
+# 1 . subset sum 
+# 2 . Equal Sum partation 
+# 3 . Count of subset sum 
+# 4 . Minium subset sum diff
+# 5 . Target sum 
+# # of teh subset & given d/f
+
+# Type of knapsack 
+    # Fractional knpasack ( can brake the item into fraction ) --> Greedy 
+    # 0 / 1 ( ya to hoga , ya to nahi , or ek he har )
+    # unbound knapseek ( 0/ 1 but ek ko bar bar dal sakte hai , multiple occurance )
+
+# 0 / 1 knapsack recursive 
+
